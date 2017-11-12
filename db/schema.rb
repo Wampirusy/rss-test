bffb6c1adca239b8311725dc7de3a0df2c0bd6f8
Anthony <wampirusy@gmail.com>
2017-11-12 12:11:10 +0200

    fixes

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20171111094520) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.string "url"
@@ -29,5 +32,4 @@ ActiveRecord::Schema.define(version: 20171111094520) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 end
